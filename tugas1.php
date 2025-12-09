dbceb77d8791257fc024f41adf32be88c5f610bb
noneng013 <pitrinoneng@gmail.com>
2025-12-09 11:04:19 +0700

    Remove tugas1.php file

--- tugas1.php
+++ tugas1.php
@@ -1,12 +1,1 @@
-<?php
-echo "Tugas 1: Array dan Foreach" . "<br>";
-$daftar_barang = ["Laptop", "Mouse", "Keyboard", "Monitor", "Webcam"];
 
-echo "Daftar Barang:" . "<br>";
-echo "<ul>";
-foreach ($daftar_barang as $barang) {
-    echo "<li>$barang</li>";
-}
-echo "</ul>";
-
-?>
